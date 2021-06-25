c708e878edf4f3817e29a7fe558bd7569e040ab6
QingpingMeng <missing1989@gmail.com>
2021-06-25 00:31:31 -0700

    test passed

--- src/raft/util.go
+++ src/raft/util.go
@@ -7,6 +7,7 @@ const Debug = false
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug {
+		log.SetFlags(log.LstdFlags | log.Lmicroseconds)
 		log.Printf(format, a...)
 	}
 	return
